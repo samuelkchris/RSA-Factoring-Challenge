9e270ef4fc96e461034f76f6f9d468898cbb11c6
Samuel Ssekizinvu <73385715+samuelkchris@users.noreply.github.com>
2022-12-25 17:29:32 +0300

    test file

--- factors.py
+++ factors.py
@@ -1,42 +1,30 @@
-import math
+import random
 
 def factorize(n):
     if n % 2 == 0:
         return (2, n // 2)
 
-    # Find a smooth number using the quadratic sieve
-    a = math.floor(math.sqrt(n))
-    b2 = a*a - n
-    b = int(math.sqrt(b2))
-    while b*b != b2:
-        a += 1
-        b2 = a*a - n
-        b = int(math.sqrt(b2))
+    x, c, y, m = random.randint(1, n - 1), random.randint(1, n - 1), random.randint(1, n - 1), 1
+    g, r, q = 1, 1, 1
+    while g == 1:
+        x = (x * x + c) % n
+        y = (y * y + c) % n
+        y = (y * y + c) % n
+        g = gcd(abs(x - y), n)
+        k += 1
+        if k % m == 0:
+            r = g
+            m *= 2
 
-    # Find the prime factors of the smooth number
-    factors = prime_factors(b2)
-
-    # Try to lift the factors of the smooth number to factors of n
-    for p in factors:
-        pe = p
-        while pe <= a:
-            pe *= p
-        x = ((pe * a) + b) // p
-        y = ((pe * a) - b) // p
-        g = gcd(x - y, n)
-        if g > 1:
-            return (g, n // g)
-
-def prime_factors(n):
-    # Find the prime factors of n using trial division
-    factors = []
-    for i in range(2, int(math.sqrt(n)) + 1):
-        while n % i == 0:
-            factors.append(i)
-            n //= i
-    if n > 1:
-        factors.append(n)
-    return factors
+    if r == n:
+        while True:
+            ys = (y * y + c) % n
+            g = gcd(abs(x - ys), n)
+            if g > 1:
+                break
+        return g, n // g
+    else:
+        return r, n // r
 
 def gcd(a, b):
     if b == 0:
